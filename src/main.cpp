399609845fcf49d2966222f3c1521258f56a82eb
Francesco Savarese <francesco@Francescos-MacBook-Pro.local>
2023-02-01 19:13:07 +0900

    first thread version

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,8 @@ class TSQueue
         {}
         TSQueue(TSQueue<T> const& otherQ)
         {
-            lock_guard lg(otherQ.mut);
+            //lock_guard lg(otherQ.mut);
+            //q = otherQ;
         }
         void push(T el)
         {
@@ -32,13 +33,12 @@ class TSQueue
             cv.wait(ul,[this]{return !q.empty();});
             value = q.front();
             q.pop();
-
         } 
         // TESTED
         shared_ptr<T> wait_and_pop()
         {
             unique_lock ul(mut);
-            cv.wait(ul, [this]{!q.empty;});
+            cv.wait(ul, [this]{!q.empty();});
             shared_ptr<T> res = make_shared<T>(q.front());
             q.pop();
             return res;
@@ -71,8 +71,37 @@ class TSQueue
             return q.empty();
         }
 };
+void feedQ(TSQueue<string> &tsq)
+{
+    bool end_found = false;
+    while(!end_found)
+    {
+        std::string el;
+        std::cout << "Insert a new word (Type end to finish): ";
+        std::cin >> el; 
+        tsq.push(el);
+        if(el == "end")
+            end_found = true;
+            
+    }
+}
+void printQ(TSQueue<string> &q)
+{
+    string val = "";
+    while(val!="end")
+    {   
+        q.wait_and_pop(val);
+        cout << "printing q value: " << val << endl;
+    }
+}
 int main()
 {
     cout << "Hello world" <<endl;
+    TSQueue<string> q;
+    thread producer(&feedQ, ref(q));
+    thread consumer(&printQ,ref(q));
+    producer.join();
+    consumer.join();
+    cout << "print Completed" << endl;
     return 0;
 }
